cea30ae0647fe2ca6f13dbaa7c95af5a85e18546
Eleton <eleton@gmail.com>
2018-08-27 14:25:30 +0200

    step 3.1 - adding styling of search field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { DomainModule } from './domain';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { MatCardModule } from '@angular/material';
 import { ProjectCardComponent } from './project-card/project-card.component';
+import { ProjectSearchComponent } from './project-search/project-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardPageComponent,
-    ProjectCardComponent
+    ProjectCardComponent,
+    ProjectSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/project-search/project-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wec-project-search',
+  templateUrl: './project-search.component.html',
+  styleUrls: ['./project-search.component.scss']
+})
+export class ProjectSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
